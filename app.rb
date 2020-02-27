49872f2313316fb976fbd29174f52132188231c8
Alex Lee <hyunho98@gmail.com>
2020-02-27 23:18:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,6 +11,6 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    params[:number].times {"#{params[:phrase]}"}
+    params[:number].to_i.times {"#{params[:phrase]}"}
   end
 end
