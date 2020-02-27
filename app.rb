9edc1859995c229fae5e700f9545389588e992b8
Alex Lee <hyunho98@gmail.com>
2020-02-27 23:37:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
       "#{params[:number1].to_i - params[:number2].to_i}"
     elsif params[:operation] == "divide"
       "#{params[:number1].to_i / params[:number2].to_i}"
-    else
+    elsif params[:operation] == "multiply"
       "#{params[:number1].to_i * params[:number2].to_i}"
     end
   end
